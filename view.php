b8c1e2a0a9e0fb1b7384c7fbb83e2773a0e0e610
Tino <calvin12968452@gmail.com>
2017-11-08 10:49:01 +0800

    test lalalaala

--- view.php
+++ view.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 //require("dbconnect.php");
-
+//拉拉拉testestsetestststse
 //set the login mark to empty
 if ( ! isset($_SESSION['uID']) or $_SESSION['uID'] <= 0) {
 	header("Location: loginForm.php");
 	exit(0);
 }
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
